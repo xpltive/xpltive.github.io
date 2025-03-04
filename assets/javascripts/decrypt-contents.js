309d21d828ca8f3d9c80a568d9d7faf6d25514a9
Luca Schneider <luca_schneider1@web.de>
2025-03-04 11:34:51 +0100

    Deployed b532054 with MkDocs version: 1.6.1

--- assets/javascripts/decrypt-contents.js
+++ assets/javascripts/decrypt-contents.js
@@ -236,7 +236,8 @@ function decryptor_reaction(key_or_keys, password_input, decrypted_content, fall
         }
 
         // continue to decrypt others parts
-        
+        let encrypted_something = {'md-nav md-nav--secondary': ['nav', 'class']};
+        decrypt_somethings(key, encrypted_something);
         if (typeof inject_something !== 'undefined') {
             decrypted_content = decrypt_somethings(key, inject_something);
         }
